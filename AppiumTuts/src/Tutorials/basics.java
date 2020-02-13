3380a92b7e7ef388a4e788da9f9a3f9c5ab6a054
Nikhilrao23 <raonikhil23@gmail.com>
2020-02-13 18:33:33 -0500

    Adding Identifying objects with xpath, Id, Class Name and Clicking -> Selecting and Clearing them

--- AppiumTuts/src/Tutorials/basics.java
+++ AppiumTuts/src/Tutorials/basics.java
@@ -1,4 +1,36 @@
 package Tutorials;
 
-public class basics {
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import java.net.MalformedURLException;
+
+public class basics extends base {
+
+    public static void main(String [] args) throws MalformedURLException {
+
+        AndroidDriver<AndroidElement> driver = Capabilities();
+
+        //Identifying Elements using XPATH, ID, Class Name using UIAutomatorviewer
+
+        driver.findElementByXPath("//android.widget.TextView[@text = 'Preference']").click();
+
+        driver.findElementByXPath("//android.widget.TextView[@text = '3. Preference dependencies']").click();
+
+        driver.findElementById("android:id/checkbox").click();
+
+        driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+
+        driver.findElementByClassName("android.widget.EditText").sendKeys("Nikhil Rao");
+
+        driver.findElementsByClassName("android.widget.Button").get(1).click();
+
+        driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+
+        driver.findElementByClassName("android.widget.EditText").clear();
+        
+
+
+    }
+
 }
