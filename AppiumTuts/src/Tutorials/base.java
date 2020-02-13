d033fd4e0b1f0a6aaf249ddd6ad073b469778363
Nikhilrao23 <raonikhil23@gmail.com>
2020-02-13 16:29:09 -0500

    Adding Capabilities instead of main

--- AppiumTuts/src/Tutorials/base.java
+++ AppiumTuts/src/Tutorials/base.java
@@ -11,23 +11,20 @@ import java.net.URL;
 
 public class base {
 
-    public static void main(String args[]) throws MalformedURLException {
+    public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException {
+
+        DesiredCapabilities cap = new DesiredCapabilities();
 
         File f = new File("src/Tutorials");
         File fs = new File(f, "ApiDemos-debug.apk");
-
-
-        DesiredCapabilities cap = new DesiredCapabilities();
 
         cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 2 XL");
         cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
         cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
 
-        //Connection to the server
-
+        //Connection Request
         AndroidDriver<AndroidElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
-
-
+        return driver;
 
     }
 }
