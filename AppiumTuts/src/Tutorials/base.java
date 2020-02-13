78cf926a660955a86f3636f68c875dc1aa58da10
Nikhilrao23 <raonikhil23@gmail.com>
2020-02-13 16:33:32 -0500

    Capabilities Function

--- AppiumTuts/src/Tutorials/base.java
+++ AppiumTuts/src/Tutorials/base.java
@@ -13,18 +13,24 @@ public class base {
 
     public static AndroidDriver<AndroidElement> Capabilities() throws MalformedURLException {
 
-        DesiredCapabilities cap = new DesiredCapabilities();
-
         File f = new File("src/Tutorials");
         File fs = new File(f, "ApiDemos-debug.apk");
+
+
+        DesiredCapabilities cap = new DesiredCapabilities();
 
         cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 2 XL");
         cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
         cap.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
 
-        //Connection Request
+        //Connection to the server
+
         AndroidDriver<AndroidElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+
         return driver;
+
+
 
     }
 }
+
